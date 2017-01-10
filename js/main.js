391630c37fba07d0ffa622c7f92538f1e8a219a0
aureliopinero@gmail.com <v4081488>
2017-01-10 20:32:30 +0900

    Carga de puntos del RDF JSON al mapa

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@
  */
 $(document).ready(function () {
 
+    //El codigo contenido aqui cargara despues de finalizar todas las demas funciones
+
     //TODO: Primero, obtener el listado de clinicas del Open Data
     //TODO: Segundo, cargar una cierta cantidad de esas clinicas como marcadores en el Mapa, limitado por el zoom y coords
     //TODO: Tercero, cuando se haga click, mostrar la informacion y poner el link a una pagina de edicion de ese punto en el mapa
     //TODO: Cuarto, pagina de edicion y evaluacion de la clinica y institucion de salud
-    alert("dentro del ready");
+
     //cargarClinicasv2();
 });
 
@@ -16,15 +18,16 @@ $(document).ready(function () {
 //////////////////////////////
 
 var posicionInicial = {lat: 35.1547072, lng: 136.9613086};
-
+var map;
 //////////////////////////////
 
 //Funcion que inicializa el mapa de Google Maps, aqui se podria poner que cargue los elementos del JSON o
 //del repositorio de Open Data
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
 
-    var map = new google.maps.Map(document.getElementById('map'), {
+    //var uluru = {lat: -25.363, lng: 131.044};
+
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 5,
         //center: uluru,
         center: posicionInicial,
@@ -37,6 +40,7 @@ function initMap() {
         streetViewControl: false
     });
 
+    /*
     var contentString = '<div id="content">'+
         '<div id="siteNotice">'+
         '</div>'+
@@ -61,6 +65,7 @@ function initMap() {
         content: contentString
     });
 
+    //Agregar el marcador ULURU
     var marker = new google.maps.Marker({
         position: uluru,
         map: map,
@@ -69,8 +74,9 @@ function initMap() {
     marker.addListener('click', function() {
         infowindow.open(map, marker);
     });
+    */
 
-    var features = [
+    /*var features = [
         {
             position: new google.maps.LatLng(-31.4039739, 148.5253239),
             type: 'info',
@@ -93,12 +99,9 @@ function initMap() {
     for (var i = 0, feature; feature = features[i]; i++) {
         //addMarker(feature, map);
         addMarkerInfoWindow(feature,map);
-    }
+    } */
 
-    //Centrar el mapa en el ultimo punto
-    map.center(features[features.length-1]);
-
-    //cargarClinicasv2();
+    cargarClinicasv2();
 }
 
 function addMarker(feature, map) {
@@ -132,87 +135,96 @@ function addMarkerInfoWindow(feature, mapa){
         map: mapa,
         title: feature.title
     });
+
+    function toggleBounce () {
+        if (marker.getAnimation() != null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    }
+
     marker.addListener('click', function() {
+        toggleBounce();
         infowindow.open(mapa, marker);
+        setTimeout(toggleBounce, 780);
     });
 
-}
-
-function cargarClinicas(){
-
-    alert('Cargar clinicas');
-    //var url = "http://linkdata.org/api/1/rdf1s4633i/herb_rdf.json";
-    var url = "res/json/nagoyahospital_rdf.json";
-
-    alert(url);
-
-    $.getJSON(url, function (json) {
-        alert("a ");
-        var au = "prueba";
-        alert(json[0].value);
-        $.each(json.list, function (i, fb) {
-            alert(fb.value);
-        });
-    });
 }
 
 function cargarClinicasv2(){
 
     var this_data_list = [];
-    //var url = "res/json/nagoyahospital_rdf.json";
-    var url = "http://linkdata.org/api/1/rdf1s268i/nagoyahospital_rdf.json?callback=?";
-    //var url = "http://linkdata.org/api/1/rdf1s3965i/hospital_list_rdf.json?callback=?";
-    alert(url);
+
+    //var url = "http://linkdata.org/api/1/rdf1s268i/nagoyahospital_rdf.json?callback=?";
+    var url = "http://linkdata.org/api/1/rdf1s3965i/hospital_list_rdf.json?callback=?";
 
     $.getJSON(url, function(data) {
         alert('Dentro del getJson');
         var subject_list = Object.keys(data);
-        alert('Subject list: ' + subject_list);
         var property_list = Object.keys(data[subject_list[0]]);
-        alert(property_list);
         var latNum = 0;
         var lngNum = 0;
 
         for (var p = 1; p < property_list.length; p++) { //1は連番だから無い
 
             var x = shortenProperty(property_list[p]);
-            alert('Valor de X es ' + x);
 
             if (x == "lat" || x == "Lat" || x == "latitude" || x == "Latitude" || x == "緯度" ||
                     x.toLocaleLowerCase().includes("lat") ) {
-                alert('Consiguio Latitud');
                 latNum = p;
             }
 
             if (x == "lng" || x == "Lng" || x == "long" || x == "longitude" || x == "Longitude" || x == "経度") {
-                alert('Consiguio Longitud');
                 lngNum = p;
             }
         }
 
         if (latNum == 0 || lngNum == 0) {
-            alert("No coordinates found");
+            alert("No se encontraron coordenadas");
         } else {
-            this_data_list.push(iconid);//アイコンのid
+            alert("Se encontraron coordenadas");
+            //this_data_list.push(iconid);//アイコンのid
             this_data_list.push(subject_list.length); //行数
             this_data_list.push(property_list.length); //列数
             this_data_list.push(latNum); //緯度の前からの位置(データ属性を除く)
             this_data_list.push(lngNum); //経度の前からの位置(データ属性を除く)
 
-            //本データプッシュ
+            //Guardar los datos en el arreglo
             for (var i = 0; i < subject_list.length; i++) {
                 var this_objects = data[subject_list[i]];
                 var this_subject_list = Object.keys(data[subject_list[i]]);
                 var this_object_list = [];
+
                 for (var j = 0; j < this_subject_list.length; j++) {
                     this_object_list.push(this_objects[this_subject_list[j]][0].value);
                 }
+
                 this_data_list.push(this_object_list);
             };
-            //LSaddTo_loaddataname(dataname);
-            //setLS(dataname, this_data_list);
-            //toastopen(dataname + "を登録しました",2000);
-            //ListHtml();
+
+            var clinicas =[];
+
+            //Generamos la lista de clinicas para el mapa
+            for (var j = 4; j < this_data_list.length; j++){
+
+                //Creamos el marcador con la data obtenida
+                var clinica = {
+                    position: new google.maps.LatLng(Number(this_data_list[j][2]), Number(this_data_list[j][3])),
+                    type: 'info',
+                    title: this_data_list[j][0],
+                    description: this_data_list[j][1]
+                }
+
+                //Agregamos al arreglo de Marcadores
+                clinicas.push(clinica);
+            }
+
+            //Ahora si, a cargarlas en el mapa
+            for (var k = 0, feature; feature = clinicas[k]; k++) {
+                addMarkerInfoWindow(feature,map);
+            }
+
         }
     });
 }
